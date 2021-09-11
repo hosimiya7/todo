3e386aa9df1434f08d7b8191d126f5586cfedf6b
hosimiya7 <nerokarurin@gmail.com>
2021-09-11 19:44:11 +0900

    タスク編集

--- config/database.php
+++ config/database.php
@@ -43,6 +43,13 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        // ★ この設定を追加
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -123,7 +130,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -26,7 +26,7 @@
                     <div class="panel-heading">タスク</div>
                     <div class="panel-body">
                         <div class="text-right">
-                            <a href="#" class="btn btn-default btn-block">
+                            <a href="{{ route('tasks.create', ['id' => $current_folder_id]) }}" class="btn btn-default btn-block">
                                 タスクを追加する
                             </a>
                         </div>
@@ -48,7 +48,7 @@
                                         <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
                                     </td>
                                     <td>{{ $task->formatted_due_date }}</td>
-                                    <td><a href="#">編集</a></td>
+                                    <td><a href="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">編集</a></td>
                                 </tr>
                             @endforeach
                         </tbody>
